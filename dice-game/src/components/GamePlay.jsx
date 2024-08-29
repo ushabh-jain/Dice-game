1bf775b4d1ddb0e7df4dd9505c1ac642edbd80b2
ushabh-jain <ushabhjain07@gmail.com>
2024-08-29 10:30:48 +0530

    khelo

---
+++ dice-game/src/components/GamePlay.jsx
@@ -0,0 +1,13 @@
+
+const GamePlay = () => {
+  return (
+    <main>
+        <div>
+            <h1>0</h1>
+            <p>Total Score</p>
+        </div>
+    </main>
+  )
+}
+
+export default GamePlay
---
+++ dice-game/src/components/StartGame.jsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+
+const StartGame = ({ toggle }) => {
+  return <Container>
+    <div>
+    <img src="/images/dices.png" alt="" />
+    </div>
+    <div className='content'>
+        <h1>Dice Game</h1>
+        <Button onClick={toggle}>Play Now</Button>
+    </div>
+  </Container>
+};
+
+export default StartGame;
+
+const Container  = styled.div`
+  max-width: 1182px;
+  height: 100vh;
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+
+  .content{
+   h1{
+     font-size:96px;
+     white-space: nowrap;
+   }
+  
+  }
+
+
+`;
+
+const Button = styled.button`
+   color: white;
+   padding:10px 18px;
+   background: #000000;
+   border-radius: 10px;
+   min-width: 220px;
+   border: none;
+   font-size: 16px;;
+   border: 1px solid transparent;
+   cursor: pointer;
+   transition:0.3s background ease-in;
+
+
+   &:hover{
+   background-color:white;
+   border:1px solid black;
+   color:black;
+   transition:0.3s background ease-in;
+   }
+
+
+
+
+
+`;
+
